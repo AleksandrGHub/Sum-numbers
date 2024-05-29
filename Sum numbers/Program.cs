7be51fc4e2eeca219bf6f1ac515f3ecf83204240
AleksandrGHub <bulanov.shura@gmail.com>
2024-05-29 22:27:05 +0300

    Добавьте файлы проекта.

---
+++ Sum numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Sum_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maxNumber = 101;
+            int randomNumber;
+            int sumMultipleNumbers = 0;
+            int multipleNumber1 = 3;
+            int multipleNumber2 = 5;
+            randomNumber = random.Next(maxNumber);
+
+            for (int i = 0; i <= randomNumber; i++)
+            {
+                if (i % multipleNumber1 == 0 | i % multipleNumber2 == 0)
+                {
+                    sumMultipleNumbers += i;
+                }
+            }
+            Console.WriteLine(sumMultipleNumbers);
+        }
+    }
+}
